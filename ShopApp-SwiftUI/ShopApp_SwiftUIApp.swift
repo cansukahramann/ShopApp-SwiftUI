7259f833a2c4450a712b1f55b3e06d6088ec2b54
cansukahramann <80894621+cansukahramann@users.noreply.github.com>
2025-02-10 14:47:43 +0300

    Initial commit

---
+++ ShopApp-SwiftUI/ShopApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopApp_SwiftUIApp.swift
+//  ShopApp-SwiftUI
+//
+//  Created by Cansu Kahraman on 10.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct ShopApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

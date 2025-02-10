8f45c308b1e1d362e4dcd87551b503ebea9b5c1a
cansukahramann <80894621+cansukahramann@users.noreply.github.com>
2025-02-10 18:52:23 +0300

    Create CategoryNameView to display category selection with horizontal scroll

---
+++ ShopApp-SwiftUI/Home/CategoryNameView.swift
@@ -0,0 +1,41 @@
+//
+//  CategoryNameView.swift
+//  ShopApp-SwiftUI
+//
+//  Created by Cansu Kahraman on 10.02.2025.
+//
+
+import SwiftUI
+
+struct CategoryNameView: View {
+    
+    var name = ["All", "Electronics","Jewelry" ,"Men's clothing",  "Women's clothing"]
+    @State private var selectedCategory: String = "All"
+    
+    var body: some View {
+        ScrollView(.horizontal,showsIndicators: false, content: {
+            LazyHStack() {
+                ForEach(name, id: \.self) { name in
+                    Button {
+                        selectedCategory = name
+                    } label: {
+                        Text(name)
+                            .font(.headline)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.pink)
+                            .underline(selectedCategory == name, color: Color.pink)
+                            .contentShape(Rectangle())
+                            .padding()
+                    }
+                }
+            }
+        })
+        .frame(height: 25)
+        .padding()
+        
+    }
+}
+
+#Preview {
+    CategoryNameView()
+}

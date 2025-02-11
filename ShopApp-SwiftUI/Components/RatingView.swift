c5f672381ea0cadc207ca72298820a984696a4b1
cansukahramann <80894621+cansukahramann@users.noreply.github.com>
2025-02-11 18:46:17 +0300

    Add RatingView for star ratings

---
+++ ShopApp-SwiftUI/Components/RatingView.swift
@@ -0,0 +1,29 @@
+//
+//  RatingView.swift
+//  ShopApp-SwiftUI
+//
+//  Created by Cansu Kahraman on 11.02.2025.
+//
+
+import SwiftUI
+
+struct RatingView: View {
+    
+    var body: some View {
+        HStack(spacing: 4) {
+            ForEach(0..<5) { _ in
+                Image(systemName: "star.fill")
+                    .frame(width: 12, height: 12)
+                    .foregroundStyle(.yellow)
+            }
+            
+            Text("(15)")
+                .foregroundStyle(.gray)
+                .font(.caption)
+        }
+    }
+}
+
+#Preview {
+    RatingView()
+}

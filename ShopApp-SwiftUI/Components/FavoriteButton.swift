0a3c8d1dbe2be6fb01c1d87b5fcdd0677cf8888b
cansukahramann <80894621+cansukahramann@users.noreply.github.com>
2025-02-11 18:47:05 +0300

    Add FavoriteButton component with like toggle

---
+++ ShopApp-SwiftUI/Components/FavoriteButton.swift
@@ -0,0 +1,35 @@
+//
+//  FavoriteButton.swift
+//  ShopApp-SwiftUI
+//
+//  Created by Cansu Kahraman on 11.02.2025.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    
+    var product: Product
+    @State private var likedProducts = [String : Bool]()
+    
+    var body: some View {
+        Button {
+            likedProducts[product.image, default: false].toggle()
+        } label: {
+            Image(systemName: likedProducts[product.image, default: false] ? "heart.fill" : "heart")
+                .resizable()
+                .frame(width: 20, height: 20)
+                .foregroundStyle(.red)
+                .shadow(radius: 5)
+                .padding(8)
+                .background {
+                    Circle()
+                        .foregroundStyle(.white)
+                }
+        }
+    }
+}
+
+#Preview {
+    FavoriteButton(product: Product(name: "Apple Watch", price: 699, image: "electronics"))
+}

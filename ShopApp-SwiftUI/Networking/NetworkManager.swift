443df3a635d5706a38a2dab50a60326c3384ae48
cansukahramann <80894621+cansukahramann@users.noreply.github.com>
2025-02-17 18:29:51 +0300

    Add NetworkManager to handle API requests with Moya

---
+++ ShopApp-SwiftUI/Networking/NetworkManager.swift
@@ -0,0 +1,33 @@
+//
+//  NetworkManager.swift
+//  ShopApp-SwiftUI
+//
+//  Created by Cansu Kahraman on 17.02.2025.
+//
+
+import Foundation
+import Moya
+
+
+final class NetworkManager {
+    private let provider = MoyaProvider<MultiTarget>()
+    
+    static let shared = NetworkManager()
+    private init() {}
+    
+    enum Error: Swift.Error {
+           case connectionError
+           case failureStatusCode
+       }
+    
+    func request(_ target: any TargetType, completion: @escaping (Result<(Data, HTTPURLResponse), Error>) -> Void) {
+        provider.request(MultiTarget(target)) { result in
+            switch result {
+            case .success(let response):
+                completion(.success((response.data, response.response!)))
+            case .failure(let error):
+                completion(.failure(Error.connectionError))
+            }
+        }
+    }
+}

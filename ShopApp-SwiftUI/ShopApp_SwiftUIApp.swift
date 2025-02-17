eafb938160475031b73878ae6d08fd67ad2b31e1
cansukahramann <80894621+cansukahramann@users.noreply.github.com>
2025-02-17 14:15:58 +0300

    Fix navigation structure in ShopApp_SwiftUIApp

--- ShopApp-SwiftUI/ShopApp_SwiftUIApp.swift
+++ ShopApp-SwiftUI/ShopApp_SwiftUIApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct ShopApp_SwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationStack {
+                HomeView()
+            }
         }
     }
 }

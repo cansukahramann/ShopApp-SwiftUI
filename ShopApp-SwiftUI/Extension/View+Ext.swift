3159fb5cf679e9b73315f0d28e2eaed0b5492369
cansukahramann <80894621+cansukahramann@users.noreply.github.com>
2025-02-17 14:14:43 +0300

    Implement View+Ext for content insets

---
+++ ShopApp-SwiftUI/Extension/View+Ext.swift
@@ -0,0 +1,24 @@
+//
+//  View+Ext.swift
+//  ShopApp-SwiftUI
+//
+//  Created by Cansu Kahraman on 17.02.2025.
+//
+
+import SwiftUI
+
+enum ContentInset {
+    case vertical(inset: CGFloat = 8)
+    case horizontal(inset: CGFloat = 8)
+}
+
+extension View {
+    func contentInsetView(_ inset: ContentInset) -> some View {
+        switch inset {
+        case .vertical(let inset):
+            Color.clear.frame(height: inset)
+        case .horizontal(let inset):
+            Color.clear.frame(width: inset)
+        }
+    }
+}

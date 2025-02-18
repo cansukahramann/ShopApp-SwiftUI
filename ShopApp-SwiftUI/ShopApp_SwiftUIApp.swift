aefa06f9f58d9f213b5ce4b49c9189c660746911
cansukahramann <80894621+cansukahramann@users.noreply.github.com>
2025-02-18 13:52:01 +0300

    Fix HomeView initialization and inject HomeViewModel properly

--- ShopApp-SwiftUI/ShopApp_SwiftUIApp.swift
+++ ShopApp-SwiftUI/ShopApp_SwiftUIApp.swift
@@ -9,10 +9,15 @@ import SwiftUI
 
 @main
 struct ShopApp_SwiftUIApp: App {
+    let homeViewModel: HomeViewModel = {
+           let service = CategoryService()
+           let viewModel = HomeViewModel(categoryService: service)
+           return viewModel
+       }()
+    
     var body: some Scene {
-        WindowGroup {
-            NavigationStack {
-                HomeView()
+        WindowGroup { NavigationStack {
+            HomeView(viewModel: homeViewModel)
             }
         }
     }

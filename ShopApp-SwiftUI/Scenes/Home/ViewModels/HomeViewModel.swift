e797cf90111419e089c4a0152d2ce0e0774b617a
cansukahramann <80894621+cansukahramann@users.noreply.github.com>
2025-02-18 12:02:11 +0300

    Refactor HomeViewModel to use Category model and CategoryService

--- ShopApp-SwiftUI/Scenes/Home/ViewModels/HomeViewModel.swift
+++ ShopApp-SwiftUI/Scenes/Home/ViewModels/HomeViewModel.swift
@@ -10,6 +10,23 @@ import Foundation
 final class HomeViewModel: ObservableObject {
     @Published private(set) var products = [Product]()
     
-    @Published private(set) var categories = [String]()
+    @Published private(set) var categories = [Category]()
     @Published var selectedCategory: Category?
+    
+    private let categoryService: CategoryService
+    
+    init(categoryService: CategoryService) {
+        self.categoryService = categoryService
+    }
+    
+    func fetchCategories() {
+        categoryService.fetchCategories { [weak self] result  in
+            switch result {
+            case .success(let model):
+                self?.categories = model.map { Category(name: $0)}
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
 }

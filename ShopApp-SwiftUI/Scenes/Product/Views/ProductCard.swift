d53be28429a4ac6e5d8c5611865f15f6e5f77b7c
cansukahramann <80894621+cansukahramann@users.noreply.github.com>
2025-02-19 16:03:14 +0300

    Refactored ProductCard view layout and removed redundant code

--- ShopApp-SwiftUI/Scenes/Product/Views/ProductCard.swift
+++ ShopApp-SwiftUI/Scenes/Product/Views/ProductCard.swift
@@ -12,59 +12,59 @@ struct ProductCard: View {
     
     let product: Product
     
+    
     var body: some View {
-        GeometryReader { geo in
-            VStack(alignment: .leading) {
-                ZStack(alignment: .topTrailing) {
-                    
-                    KFImage(URL(string: product.image))
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: geo.size.width, height: 150)
-                        .cornerRadius(10)
-                        .shadow(radius: 5)
-                        .clipped()
-                    
-                    FavoriteButton(isFavorite: Bool.random()) {}
-                }
+        
+        VStack(alignment: .leading) {
+            ZStack(alignment: .topTrailing) {
                 
-                VStack(alignment: .leading) {
+                KFImage(URL(string: product.image))
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 180, height: 130)
+                    .cornerRadius(10)
+                    .shadow(radius: 5)
+                    .clipped()
+                
+                FavoriteButton(isFavorite: Bool.random()) {}
+            }
             
-                    RatingView(product: product)
+            VStack(alignment: .leading) {
+                
+                RatingView(product: product)
+                
+                Text(product.title)
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(Color.customNavyBlue)
+                
+                HStack {
+                    Text("$350")
+                        .font(.caption)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.gray)
+                        .strikethrough()
                     
-                    Text(product.title)
-                        .font(.footnote)
+                    Spacer()
+                    
+                    Text("$\(product.price, specifier:"%.2f")")
+                        .font(.caption)
                         .fontWeight(.semibold)
                         .foregroundStyle(Color.customNavyBlue)
-                    
-                    HStack {
-                        Text("$350")
-                            .font(.caption)
-                            .fontWeight(.semibold)
-                            .foregroundStyle(.gray)
-                            .strikethrough()
-                        
-                        Spacer()
-                        
-                        Text("$\(product.price, specifier:"%.2f")")
-                            .font(.caption)
-                            .fontWeight(.semibold)
-                            .foregroundStyle(Color.customNavyBlue)
-                    }
                 }
-                .padding()
-                Spacer()
             }
-            .frame(maxHeight: .infinity)
-            .overlay {
-                RoundedRectangle(cornerRadius: 12)
-                    .stroke(lineWidth: 1)
-                    .fill(Color(.lightGray))
-                    .shadow(radius: 5)
-            }
+            .padding()
+            Spacer()
         }
-        .frame(height: 250)
+        .frame(width: 180)
+        .overlay {
+            RoundedRectangle(cornerRadius: 16)
+                .stroke(lineWidth: 1)
+                .fill(Color(.lightGray))
+            
+        }
     }
+    
 }
 
 

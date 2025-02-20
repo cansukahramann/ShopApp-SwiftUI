ba99f5890c66de0e59f28cc3d718cc375d37da6d
cansukahramann <80894621+cansukahramann@users.noreply.github.com>
2025-02-20 18:42:02 +0300

    Enhance category selection logic in HomeViewModel

--- ShopApp-SwiftUI/Scenes/Home/ViewModels/HomeViewModel.swift
+++ ShopApp-SwiftUI/Scenes/Home/ViewModels/HomeViewModel.swift
@@ -11,7 +11,14 @@ final class HomeViewModel: ObservableObject {
     @Published private(set) var products = [Product]()
     
     @Published private(set) var categories = [Category]()
-    @Published var selectedCategory: Category?
+    @Published var selectedCategory: Category? {
+        didSet {
+            if let selectedCategory {
+                fetchProductsByCategory(category: selectedCategory)
+            }
+        }
+    }
+    
     private let dataLoader: HomeDataLoader
     
     init(dataLoader: HomeDataLoader = .init()) {
@@ -32,13 +39,24 @@ final class HomeViewModel: ObservableObject {
         }
     }
     
-    func fetchProductsByCategory(category: Category) {
-        dataLoader.fetchProductsByCategory(categoryName: category.name) { [weak self] result in
-            switch result {
-            case .success(let data):
-                self?.products = data
-            case .failure(let error):
-                print(error)
+    private func fetchProductsByCategory(category: Category) {
+        if category.name == "All" {
+            dataLoader.fetchData { [weak self] result in
+                switch result {
+                case .success(let data):
+                    self?.products = data.products
+                case .failure(let error):
+                    print(error)
+                }
+            }
+        } else {
+            dataLoader.fetchProductsByCategory(categoryName: category.name) { [weak self] result in
+                switch result {
+                case .success(let data):
+                    self?.products = data
+                case .failure(let error):
+                    print(error)
+                }
             }
         }
     }

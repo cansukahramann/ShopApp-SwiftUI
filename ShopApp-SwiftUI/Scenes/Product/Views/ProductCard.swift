201d0be31bc7e38a9f49a6c7c12232582d324015
cansukahramann <80894621+cansukahramann@users.noreply.github.com>
2025-02-20 19:51:51 +0300

    Update ProductCard layout and add favorite button

--- ShopApp-SwiftUI/Scenes/Product/Views/ProductCard.swift
+++ ShopApp-SwiftUI/Scenes/Product/Views/ProductCard.swift
@@ -11,33 +11,32 @@ import Kingfisher
 struct ProductCard: View {
     
     let product: Product
-    
-    
+
     var body: some View {
         
-        VStack(alignment: .leading) {
+        VStack {
             ZStack(alignment: .topTrailing) {
-                
                 KFImage(URL(string: product.image))
                     .resizable()
                     .aspectRatio(contentMode: .fill)
-                    .frame(width: 130, height: 100)
+                    .frame(height: 120)
                     .cornerRadius(10)
                     .shadow(radius: 5)
                     .clipped()
-                    .debugBackground()
                 
                 FavoriteButton(isFavorite: Bool.random()) {}
             }
             
             VStack(alignment: .leading) {
                 
-                RatingView(product: product)
-                
                 Text(product.title)
                     .font(.footnote)
                     .fontWeight(.semibold)
                     .foregroundStyle(Color.customNavyBlue)
+                
+                RatingView(product: product)
+                
+               
                 
                 HStack {
                     Text("$350")
@@ -61,8 +60,6 @@ struct ProductCard: View {
     }
     
 }
-
-
 
 #Preview {
     ProductCard(product: Product(id: 1, title: "Apple Watch", price: 699, image: "electronics", category: "Electronics", description: "sdlsdlsldlsdlsldldlsldlsdlsdllsdlsdlsldlds", rating: Rating(rate: 5.0, count: 5)))

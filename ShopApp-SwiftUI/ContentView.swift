953cee73f51f1603b4103b6af57dd32ff7123bb7
cansukahramann <80894621+cansukahramann@users.noreply.github.com>
2025-02-23 21:45:46 +0300

    Added onboarding screen

--- ShopApp-SwiftUI/ContentView.swift
+++ ShopApp-SwiftUI/ContentView.swift
@@ -12,23 +12,100 @@ struct ContentView: View {
     @StateObject private var cartViewModel = CartViewModel()
     @StateObject private var profileViewModel = ProfileViewModel()
     
+    @State private var showOnboard: Bool = true
+    
     var body: some View {
-        TabView {
-            HomeView(viewModel: homeViewModel)
-                .tabItem {
-                    Image(systemName: "house")
-                    Text("Home")
+        if showOnboard {
+            OnboardView(showOnboard: $showOnboard)
+        } else {
+            TabView {
+                HomeView(viewModel: homeViewModel)
+                    .tabItem {
+                        Image(systemName: "house")
+                        Text("Home")
+                    }
+                CartView(viewModel: cartViewModel)
+                    .tabItem {
+                        Image(systemName: "cart")
+                        Text("Cart")
+                    }
+                ProfileView(viewModel: profileViewModel)
+                    .tabItem {
+                        Image(systemName: "person")
+                        Text("Profile")
+                    }
+            }
+        }
+        
+    }
+}
+
+struct OnboardView: View {
+    
+    @Binding var showOnboard: Bool
+    @State private var selectedTab = 0
+    
+    var body: some View {
+        TabView(selection: $selectedTab) {
+            OnboardScreen(imageName: "OnboardView1")
+                .tag(0)
+            OnboardScreen(imageName: "OnboardView2")
+                .tag(1)
+            LastOnboardScreen(showOnboard: $showOnboard)
+                .tag(2)
+        }
+        .tabViewStyle(.page(indexDisplayMode: .always))
+        .onAppear {
+            UIPageControl.appearance().currentPageIndicatorTintColor = .black
+            UIPageControl.appearance().pageIndicatorTintColor = .brown
+        }
+    }
+}
+
+struct OnboardScreen: View {
+    var imageName: String
+    
+    var body: some View {
+        VStack {
+            Image(imageName)
+                .resizable()
+                .scaledToFill()
+                .frame(height: UIScreen.main.bounds.height * 0.3)
+                .ignoresSafeArea(edges: .top)
+                
+        }
+    }
+}
+
+struct LastOnboardScreen: View {
+    
+    @Binding var showOnboard: Bool
+    
+    var body: some View {
+        ZStack {
+            Image("OnboardView3")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .ignoresSafeArea(.all)
+            
+            VStack() {
+                Spacer()
+                Button {
+                    showOnboard = false
+                } label: {
+                    Text("Start Shopping")
+                        .fontWeight(.bold)
+                        .padding()
+                        .frame(width: 200, height: 50)
+                        .background(.brown)
+                        .foregroundStyle(.white)
+                        .clipShape(.buttonBorder)
+                        .shadow(radius: 3)
                 }
-            CartView(viewModel: cartViewModel)
-                .tabItem {
-                    Image(systemName: "cart")
-                    Text("Cart")
-                }
-            ProfileView(viewModel: profileViewModel)
-                .tabItem {
-                    Image(systemName: "person")
-                    Text("Profile")
-                }
+                .padding(.bottom, 100)
+                
+            }
         }
     }
 }

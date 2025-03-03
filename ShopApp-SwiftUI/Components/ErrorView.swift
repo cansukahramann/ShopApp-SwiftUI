4135a39f461c7462d99f276ffa3c1b7416a07363
cansukahramann <80894621+cansukahramann@users.noreply.github.com>
2025-03-03 16:38:56 +0300

    Add ErrorView component for handling error states in ViewState

---
+++ ShopApp-SwiftUI/Components/ErrorView.swift
@@ -0,0 +1,16 @@
+//
+//  ErrorView.swift
+//  ShopApp-SwiftUI
+//
+//  Created by Cansu Kahraman on 3.03.2025.
+//
+
+import SwiftUI
+
+struct ErrorView: View {
+    let message: String
+    
+    var body: some View {
+        Text(message)
+    }
+}

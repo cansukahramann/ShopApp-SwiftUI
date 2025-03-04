58ee12dd4b52502a3ab27385c8f0619974b88841
cansukahramann <80894621+cansukahramann@users.noreply.github.com>
2025-03-04 12:51:37 +0300

    Refactor HomeView to integrate CategoryListViewFactory and ProductListingViewFactory

--- ShopApp-SwiftUI/Scenes/Home/Views/HomeView.swift
+++ ShopApp-SwiftUI/Scenes/Home/Views/HomeView.swift
@@ -10,24 +10,39 @@ import SwiftUI
 struct HomeView: View {
     @ObservedObject var viewModel: HomeViewModel
     @State private var selectedProduct: Product?
+    @State private var isCategoryLoadFinished = false
+    @State private var isProductLoadFinished = false
+    
+    @State private var selectedCategory: Category?
+    
+    private var isLoading: Bool {
+        !isProductLoadFinished || !isCategoryLoadFinished
+    }
     
     var body: some View {
         ScrollView {
             LazyVStack(spacing: 12) {
                 BannerListView()
                 
-//                CategoryListView(viewModel)
+                CategoryListViewFactory.makeView {
+                    isCategoryLoadFinished = true
+                } onSelectedCategoryChange: { category in
+                    selectedCategory = category
+                }
+
                 
-                ProductListView(products: viewModel.products, selectedProduct: $selectedProduct)
-                    .padding()
-                
-                if viewModel.isLoading {
-                    LoadingIndicatorView()
+                ProductListingViewFactory.makeView(category: $selectedCategory) {
+                    isProductLoadFinished = true
                 }
                 
             }
             .onViewDidLoad {
                 viewModel.fetchIntialData()
+            }
+        }
+        .overlay {
+            if  isLoading {
+                LoadingIndicatorView()
             }
         }
     }
